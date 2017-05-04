6923d6df67637d5c3edd547f82c31ccafdaa3f8c
Andrewzalivako <drus0682@gmail.com>
2017-05-04 12:57:59 +0300

    Add files via upload

---
+++ Lab6/Lab5.2.10_Zalivako/Lab5.2.10_Zalivako/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5._2._10_Zalivako
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+            for(;;)
+            {
+                string word = Console.ReadLine();
+                if (word == "end")
+                    break;
+                try
+                {
+                    tree.Insert(word);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+            }
+
+            Console.WriteLine("\n\n\n");
+
+            try
+            {
+                tree.Display(tree);
+            }
+            catch(NullReferenceException)
+            {
+                Console.WriteLine("Пустое дерево");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab6/Lab5.2.10_Zalivako/Lab5.2.10_Zalivako/Tree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5._2._10_Zalivako
+{
+    class Tree
+    {
+        private string value;
+        private Tree left;
+        private Tree right;
+
+        // вставка
+        public void Insert(string value)
+        {
+            if (this.value == null)
+                this.value = value;
+            else
+            {
+                if (this.value.CompareTo(value) == 1)
+                {
+                    if (left == null)
+                        this.left = new Tree();
+                    left.Insert(value);
+                }
+                else if (this.value.CompareTo(value) == -1)
+                {
+                    if (right == null)
+                        this.right = new Tree();
+                    right.Insert(value);
+                }
+                else
+                    throw new Exception("Узел уже существует");
+            }
+
+        }
+
+        public void Display(Tree tree)
+        {
+            if (tree != null)
+            {
+                Display(tree.left); //Инфиксный обход.
+
+                string RevVal = "";
+                for(int i = tree.value.Length - 1; i >= 0; i--)
+                {
+                    RevVal += tree.value[i];
+                }
+                if (tree.value == RevVal)
+                {
+                    Console.WriteLine(tree.value);
+                }
+
+                Display(tree.right);
+            }
+        }
+    }
+    }
